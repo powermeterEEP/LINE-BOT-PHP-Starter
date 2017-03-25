d716e7b56e1953dd98abe89373d0a9624d109d8c
powermeterEEP <wachara77@hotmail.co.th>
2017-03-25 17:15:54 +0700

    Update BOT.php

--- BOT.php
+++ BOT.php
@@ -18,7 +18,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
- echo "OK";
+  echo "OK";
 }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
@@ -35,7 +35,6 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
- 
  
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
