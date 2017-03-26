bfad1d0f2876b9516d26eb25e0e94b2bb9d81298
powermeterEEP <wachara77@hotmail.co.th>
2017-03-26 15:37:52 +0700

    Update BOT.php

--- BOT.php
+++ BOT.php
@@ -19,7 +19,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
- echo 'Succeeded!';
+ echo "HelloT";
 }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
