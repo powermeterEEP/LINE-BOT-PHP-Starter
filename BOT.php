39e57792433a04fd409f1a30c8c228344284bc2f
powermeterEEP <wachara77@hotmail.co.th>
2017-03-26 16:22:26 +0700

    Update BOT.php

--- BOT.php
+++ BOT.php
@@ -36,9 +36,6 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
 
-if(['message']['text'] == "สวัสดี"){
-echo "Hello...............123.";}
-
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
